c0b1acaa8b66a2205df38a83addcedfbe0d0f671
White Tiger <dayley.zimmermann@gmail.com>
2022-01-31 15:32:23 -0500

    license badge render function done

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
+//licenses to include : MIT Apache 2.0 GPLv3 and BSD 3
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license !== ''){
+    return `[![License](https://img.shields.io/badge/license-${license}-blue.svg)]`
+  }
+  return '';
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
